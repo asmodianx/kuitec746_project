c2b72f7e33c4f205f6aaa327554ed483544c63db
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-02 17:26:18 -0600

    Add files via upload
    #TODO: df and dmd - the insert statements for DMD needs to check to see if the file guid and ifm guid exists before inserting
    #todo: create sql to return GUID exists for all tables then run it for updates inserts
    
    Also my functions likely are not right.  I may need to rename the variables but at least its now in a somewhat consistent state.

---
+++ document_repository.py
@@ -0,0 +1,481 @@
+import uuid
+from typing import List
+
+from models.document_model import Document, DocumentMeta
+from repositories.db_connection import DBConnection
+
+# Documents
+#GET_ALL_FILE_BY_API = 'select A."API",A."APIGUID",A."OwnerEmailAddress",B."Name",B."Description",B."DateModified",B."DateUploaded",B."Revision",B."Name",B."IsCurrent",D."FileMIMEType",D."FileExtension",C."fileGuid" from "kershner"."API" A, "kershner"."FileMetadata" B,"kershner"."InputFile" C,"kershner"."InputFileFormat" D WHERE A."APIGUID" = %s AND A."APIGUID" = B."APIGUID" AND B."FileGuid" = C."fileGuid" AND  B."InputFormatGuid" = D."InputFormatGuid" AND A."IsDeleted" = false AND A."IsDisabled" = false AND A."DateExpires"::date > current_date AND B."IsCurrent" = true AND B."IsDeleted" = false;'
+#INSERT_FMD = 'INSERT INTO kershner."FileMetadata"("APIGUID", "FileMetaDataGUID", "Name", "Description", "DateUploaded", "DateModified", "Revision", "IsCurrent", "IsDeleted", "FileGuid", "InputFormatGuid", "FileHash", "FileCacheExpiration") SELECT %s as "APIGUID", %s as "FileMetaDataGUID", %s as "Name", %s as "Description", CURRENT_DATE as "DateUploaded", CURRENT_DATE as "DateModified", '1' as "Revision", true as "IsCurrent", false as "IsDeleted", "fileGuid" as "fileGuid", %s as "InputFormatGuid", md5("fileContents")::text as fileHash, %s as "FileCacheExpiration" FROM kershner."InputFile" WHERE "fileGuid" = %s;'
+#INSERT_FILE = 'INSERT INTO kershner."InputFile"("fileGuid", "fileContents") values(%s,%s);'
+#GET_FILE_BY_ID = 'select A."API",A."OwnerEmailAddress",B."FileMetaDataGUID",B."Name",B."Description",B."DateModified",B."DateUploaded",B."Revision",B."Name",B."IsCurrent",D."FileMIMEType",D."FileExtension",C."fileContents" from "kershner"."API" A,"kershner"."FileMetadata" B,"kershner"."InputFile" C,"kershner"."InputFileFormat" D WHERE A."APIGUID" = %s AND B."FileMetaDataGUID" = %s A."APIGUID" = B."APIGUID" AND B."FileGuid" = C."fileGuid" AND B."InputFormatGuid" = D."InputFormatGuid" AND A."IsDeleted" = false AND A."IsDisabled" = false AND A."DateExpires"::date > current_date AND B."IsCurrent" = true AND B."IsDeleted" = false;'
+#UPDATE_FILE= "UPDATE users set first_name = %s, last_name=%s WHERE user_id = %s"
+#DELETE_FILE = "DELETE FROM users where user_id=%s"
+
+# Document file DF
+SELECT_ALL_FILES = 'SELECT "fileGuid", "fileContents" FROM "kershner"."InputFile";'
+SELECT_FILE = 'SELECT "fileGuid", "fileContents" FROM "kershner"."InputFile" WHERE "fileGuid" = %s;'
+INSERT_FILE = 'INSERT INFO "kershner"."InputFile" "fileGuid", "fileContents" values(%s,%s);'
+UPDATE_FILE = 'UPDATE "kershner"."InputFile" "fileContents" = %s WHERE "fileGuid" = %s;'
+DELETE_FILE = 'DELETE * FROM "kershner"."InputFile" WHERE "fileGuid" = %s;'
+
+# DOCUMENT META DATA DMD
+SELECT_ALL_MD = 'SELECT "APIGUID", "FileMetaDataGUID", "Name", "Description", "DateUploaded", "DateModified", "Revision", "IsCurrent", "IsDeleted", "FileGuid", "InputFormatGuid", "FileHash", "FileCacheExpiration" FROM "kershner"."FileMetadata";'
+SELECT_MD = 'SELECT "APIGUID", "FileMetaDataGUID", "Name", "Description", "DateUploaded", "DateModified", "Revision", "IsCurrent", "IsDeleted", "FileGuid", "InputFormatGuid", "FileHash", "FileCacheExpiration" FROM "kershner"."FileMetadata" WHERE "FileMetaDataGUID"=%s;'
+INSERT_MD = 'INSERT INTO "kershner"."FileMetadata" "APIGUID", "FileMetaDataGUID", "Name", "Description", "DateUploaded", "DateModified", "Revision", "IsCurrent", "IsDeleted", "FileGuid", "InputFormatGuid", "FileHash", "FileCacheExpiration" values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+UPDATE_MD = 'UPDATE "kershner"."FileMetadata" SET "APIGUID" = %s, "Name" = %s, "Description" = %s, "DateUploaded" = %s, "DateModified" = %s, "Revision", "IsCurrent" = %s, "IsDeleted" = %s, "FileGuid" = %s, "InputFormatGuid" = %s, "FileHash" = %s, "FileCacheExpiration" = %s WHERE "FileMetaDataGUID";'
+DELETE_MD = 'UPDATE "kershner"."FileMetadata" SET "IsDeleted" = true where "FileMetaDataGUID"=%s;'
+PURGE_MD = 'DELETE * FROM "kershner"."FileMetadata" where "FileMetaDataGUID"=%s;'
+
+# input format IFM
+SELECT_ALL_IF = 'SELECT "InputFormatGuid", "Name", "Description", "FileExtension", "FileMIMEType" FROM "kershner"."InputFileFormat";'
+SELECT_IF = 'SELECT "InputFormatGuid", "Name", "Description", "FileExtension", "FileMIMEType" FROM "kershner"."InputFileFormat" WHERE InputFormatGuid=%s;'
+INSERT_IF = 'INSERT INTO "kershner"."InputFileFormat" "InputFormatGuid", "Name", "Description", "FileExtension", "FileMIMEType" VALUES(GEN_RANDOM_UUID(),%s,%s,%s,%s);'
+UPDATE_IF = 'UPDATE "kershner"."InputFileFormat" SET "Name"=%s, "Description"=%s, "FileExtension"=%s, "FileMIMEType" = %s WHERE InputFormatGuid=%s;'
+DELETE_IF = 'DELETE FROM "kershner"."InputFileFormat" WHERE InputFormatGuid=%s;'
+
+# output format OFM
+SELECT_ALL_OF = 'SELECT "OutputFormatGuid", "Name", "Description", "FunctionName", "FileExtension", "FileMIMEType" FROM "kershner"."DataOutputFormat";'
+SELECT_OF = 'SELECT "OutputFormatGuid", "Name", "Description", "FunctionName", "FileExtension", "FileMIMEType" FROM "kershner"."DataOutputFormat" WHERE "OutputFormatGuid"=%s;'
+INSERT_OF = 'INSERT INTO "kershner"."DataOutputFormat" "OutputFormatGuid", "Name", "Description", "FunctionName", "FileExtension", "FileMIMEType" VALUES(GEN_RANDOM_UUID(),%s,%s,%s,%s,%s);'
+UPDATE_OF = 'UPDATE "kershner"."DataOutputFormat" SET "Name" = %s, "Description" = %s, "FunctionName" = %s, "FileExtension" = %s, "FileMIMEType" = %s WHERE "OutputFormatGuid"=%s;'
+DELETE_OF = 'DELETE * FROM "kershner"."DataOutputFormat" WHERE "OutputFormatGuid"=%s;'
+
+# API API
+SELECT_ALL_API = 'SELECT "APIGUID", "API", "OwnerEmailAddress", "DateExpires", "PermissionCreate", "PermissionGlobalAdmin", "IsDisabled", "IsDeleted" FROM "kershner"."API";'
+SELECT_API = 'SELECT "APIGUID", "API", "OwnerEmailAddress", "DateExpires", "PermissionCreate", "PermissionGlobalAdmin", "IsDisabled", "IsDeleted" FROM "kershner"."API" WHERE APIGUID=%s;'
+INSERT_API = 'INSERT INTO "kershner"."API" "APIGUID", "API", "OwnerEmailAddress", "DateExpires", "PermissionCreate", "PermissionGlobalAdmin", "IsDisabled", "IsDeleted" VALUES (GENERATE_RANDOM_UUID(),%s,%s,%s,%s,%s,false,false);'
+UPDATE_API = 'UPDATE "kershner"."API" SET "API" = %s, "OwnerEmailAddress" = %s, "DateExpires" = %s, "PermissionCreate" = %s, "PermissionGlobalAdmin" = %s, "IsDisabled" = %s, "IsDeleted" = %s WHERE "APIGUID" = %s;'
+DELETE_API = 'UPDATE "kershner"."API" SET IsDisabled = true,IsDeleted = true WHERE "APIGUID" = %s;'
+PURGE_API = 'DELETE * FROM "kershner"."API" WHERE "APIGUID" = %s;'
+
+# search SR
+SELECT_ALL_SEARCH = 'SELECT "SearchGUID", "Name", "Description" FROM "kershner"."Search";'
+SELECT_SEARCH = 'SELECT "SearchGUID", "Name", "Description" FROM "kershner"."Search" where "SearchGUID" = %s;'
+INSERT_SEARCH = 'INSERT INTO "kershner"."Search" "SearchGUID", "Name", "Description" VALUES(GENERATE_RANDOM_UUID(),%s,%s);'
+UPDATE_SEARCH = 'UPDATE "SearchGUID", "Name", "Description" FROM "kershner"."Search";'
+DELETE_SEARCH = 'DELETE * FROM "kershner"."Search" WHERE "SearchGUID" = %s;'
+
+
+
+def get_all_df() -> List[df]:
+    df = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_ALL_FILES)
+#"fileGuid", "fileContents"
+    for row in cur:
+        df.append(
+        	df(
+				fileGuid=str(row[0]),
+				fileContents=str(row[1])
+        	)
+    return df
+
+def get_all_dmd() -> List[dmd]:
+    df = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_ALL_MD)
+	#"APIGUID", "FileMetaDataGUID", "Name", "Description", "DateUploaded", "DateModified", "Revision", "IsCurrent", "IsDeleted", "FileGuid", "InputFormatGuid", "FileHash", "FileCacheExpiration"
+    for row in cur:
+        dmd.append(
+        	dmd(
+				APIGUID=str(row[0]),
+				FileMetaDataGUID=str(row[1]),
+				Name=str(row[2]),
+				Description=str(row[3]),
+				DateUploaded=str(row[4]),
+				DateModified=str(row[5]),
+				Revision=str(row[6]),
+				IsCurrent=bool(row[7]),
+				IsDeleted=bool(row[8]),
+				FileGuid=str(row[9]),
+				InputFormatGuid=str(row[10]),
+				FileHash=str(row[11]),
+				FileCacheExpiration=str(row[12])
+        		)
+    return dmd
+
+def get_df(did: uuid) -> List[df]:
+    df = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_FILE, did)
+#"fileGuid", "fileContents"
+    for row in cur:
+        df.append(
+        	df(
+				fileGuid=str(row[0]),
+				fileContents=str(row[1])
+        		)
+    return df
+
+def get_dmd(did: uuid) -> List[dmd]:
+    df = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_MD, did)
+	#"APIGUID", "FileMetaDataGUID", "Name", "Description", "DateUploaded", "DateModified", "Revision", "IsCurrent", "IsDeleted", "FileGuid", "InputFormatGuid", "FileHash", "FileCacheExpiration"
+    for row in cur:
+        dmd.append(
+        	dmd(
+				APIGUID=str(row[0]),
+				FileMetaDataGUID=str(row[1]),
+				Name=str(row[2]),
+				Description=str(row[3]),
+				DateUploaded=str(row[4]),
+				DateModified=str(row[5]),
+				Revision=str(row[6]),
+				IsCurrent=bool(row[7]),
+				IsDeleted=bool(row[8]),
+				FileGuid=str(row[9]),
+				InputFormatGuid=str(row[10]),
+				FileHash=str(row[11]),
+				FileCacheExpiration=str(row[12])
+        		)
+    return dmd
+
+def create_df(df: df) -> df:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.uuid4()
+    df.fileGuid = did
+    cur.execute(INSERT_FILE, (
+		df.fileGuid, 
+    	df.fileContents
+    	)
+    )
+    db.connection.commit()
+    return get_df(did)
+
+def create_dmd(dmd: dmd) -> dmd:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.uuid4()
+    dmd.FileMetaDataGUID = did
+    cur.execute(INSERT_MD, (
+		dmd.APIGUID, 
+    	dmd.FileMetaDataGUID, 
+    	dmd.Name, 
+    	dmd.Description, 
+    	dmd.DateUploaded, 
+    	dmd.DateModified, 
+    	dmd.Revision, 
+    	dmd.IsCurrent, 
+   		dmd.IsDeleted, 
+    	dmd.FileGuid, 
+    	dmd.InputFormatGuid, 
+    	dmd.FileHash, 
+    	dmd.FileCacheExpiration  
+   		)
+   	)
+    db.connection.commit()
+    return get_dmd(did)
+
+def update_df(df: df) -> df:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.UUID(df.fileGuid)
+    cur.execute(UPDATE_FILE, (did, df.fileContents))
+    db.connection.commit()
+    return get_df(did)
+
+def update_dmd(dmd: dmd) -> dmd:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.UUID(dmd.FileMetaDataGUID)
+    cur.execute(UPDATE_MD, (did, dmd.fileContents))
+    db.connection.commit()
+    return get_dmd(did)
+
+def delete_df(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(DELETE_FILE, [did])
+    db.connection.commit()
+    return
+    
+def delete_dmd(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(DELETE_MD, [did])
+    db.connection.commit()
+    return
+
+def purge_dmd(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(PURGE_MD, [did])
+    db.connection.commit()
+    return
+
+#==== input format
+def get_all_ifm() -> List[ifm]:
+    df = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_ALL_IF)
+	#"InputFormatGuid", "Name", "Description", "FileExtension", "FileMIMEType"
+    for row in cur:
+        ifm.append(
+        	ifm(
+				InputFormatGuid=str(row[0]),
+				Name=str(row[1]),
+				Description=str(row[2]),
+				FileExtension=str(row[3]),
+				FileMIMEType=str(row[4])
+        		)
+    return ifm
+
+def get_ifm(did: uuid) -> List[ifm]:
+    df = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_IF, did)
+    for row in cur:
+        ifm.append(
+        	ifm(
+				InputFormatGuid=str(row[0]),
+				Name=str(row[1]),
+				Description=str(row[2]),
+				FileExtension=str(row[3]),
+				FileMIMEType=str(row[4])
+        		)
+    return df
+
+def create_ifm(ifm: ifm) -> ifm:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.uuid4()
+    ifm.InputFormatGuid = did
+    cur.execute(INSERT_IF, (
+		ifm.InputFormatGuid, 
+    	ifm.Name,
+    	ifm.Description,
+    	ifm.FileExtension,
+    	ifm.FileMIMEType
+    	)
+    )
+    db.connection.commit()
+    return get_ifm(did)
+
+def update_ifm(ifm: ifm) -> ifm:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.UUID(ifm.InputFormatGuid)
+    cur.execute(UPDATE_IF, (did, ifm.Name, ifm.Description, ifm.FileExtension, ifm.FileMIMEType))
+    db.connection.commit()
+    return get_ifm(did)
+
+def delete_ifm(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(DELETE_IF, [did])
+    db.connection.commit()
+    return
+    
+#==== output format
+#"OutputFormatGuid", "Name", "Description", "FunctionName", "FileExtension", "FileMIMEType" 
+def get_all_ofm() -> List[ofm]:
+    documents = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_ALL_OF)
+
+    for row in cur:
+        ofm.append(
+        	ofm(
+				OutputFormatGuid=str(row[0]),
+				Name=str(row[1]),
+        		Description=str(row[2]), 
+        		FunctionName=str(row[3]), 
+        		FileExtension=str(row[4]),
+				FileMIMEType=str(row[5])
+        		)
+    return ofm
+def get_ofm(did: uuid) -> List[ofm]:
+    df = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_OF, did)
+    for row in cur:
+        ofm.append(
+        	ofm(
+				OutputFormatGuid=str(row[0]),
+				Name=str(row[1]),
+        		Description=str(row[2]), 
+        		FunctionName=str(row[3]), 
+        		FileExtension=str(row[4]),
+				FileMIMEType=str(row[5])
+        		)
+    return df
+
+def create_ofm(ofm: ofm) -> ofm:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.uuid4()
+    ofm.OutputFormatGuid = did
+    cur.execute(INSERT_OF, (
+		ofm.OutputFormatGuid, 
+    	ofm.Name,
+    	ofm.FunctionName,
+    	ofm.FileExtension,
+    	ofm.FileMIMEType
+    	)
+    )
+    db.connection.commit()
+    return get_ofm(did)
+
+def update_ofm(ofm: ofm) -> ofm:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.UUID(ofm.OutputFormatGuid)
+    cur.execute(UPDATE_OF, (did, ofm.Name, ofm.FunctionName, ofm.FileExtension, ofm.FileMIMEType))
+    db.connection.commit()
+    return get_ofm(did)
+
+def delete_ofm(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(DELETE_OF, [did])
+    db.connection.commit()
+    return
+    
+#=== API
+#"APIGUID", "API", "OwnerEmailAddress", "DateExpires", "PermissionCreate", "PermissionGlobalAdmin", "IsDisabled", "IsDeleted"
+def get_all_api() -> List[api]:
+    documents = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_ALL_API)
+    for row in cur:
+        api.append(
+        	api(
+				APIGUID=str(row[0]),
+				API=str(row[1]),
+        		OwnerEmailAddress=str(row[2]), 
+        		DateExpires=str(row[3]), 
+        		PermissionCreate=bool(row[4]),
+				PermissionGlobalAdmin=bool(row[5]),
+				IsDisabled=bool(row[6]),
+				IsDeleted=bool(row[7])
+        		)
+    return api    
+def get_api(did: uuid) -> List[api]:
+    api = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_API, did)
+    for row in cur:
+        api.append(
+        	api(
+				APIGUID=str(row[0]),
+				API=str(row[1]),
+        		OwnerEmailAddress=str(row[2]), 
+        		DateExpires=str(row[3]), 
+        		PermissionCreate=bool(row[4]),
+				PermissionGlobalAdmin=bool(row[5]),
+				IsDisabled=bool(row[6]),
+				IsDeleted=bool(row[7])
+        		)
+    return api
+
+def create_api(api: api) -> api:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.uuid4()
+    api.OutputFormatGuid = did
+    cur.execute(INSERT_API, (
+		api.APIGUID, 
+    	api.API,
+    	api.OwnerEmailAddress,
+    	api.DateExpires,
+    	api.PermissionCreate,
+    	api.PermissionGlobalAdmin,
+    	api.IsDisabled,
+    	api.IsDeleted
+    	)
+    )
+    db.connection.commit()
+    return get_api(did)
+
+def update_api(api: api) -> api:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.UUID(api.OutputFormatGuid)
+    cur.execute(UPDATE_API, (did, api.API, api.OwnerEmailAddress, api.DateExpires, api.PermissionCreate, api.PermissionGlobalAdmin, api.IsDisabled, api.IsDeleted))
+    db.connection.commit()
+    return get_api(did)
+
+def delete_api(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(DELETE_API, [did])
+    db.connection.commit()
+    return    
+
+def purge_api(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(PURGE_API, [did])
+    db.connection.commit()
+    return    
+
+#==== search
+def get_all_sr() -> List[sr]:
+    documents = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_ALL_SEARCH)
+    #"SearchGUID", "Name", "Description"
+    for row in cur:
+        sr.append(
+        	sr(
+				SearchGUID=str(row[0]),
+				Name=str(row[1]),
+        		Description=str(row[2])
+        		)
+    return sr
+def get_sr(did: uuid) -> List[sr]:
+    sr = []
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(SELECT_SEARCH, did)
+    for row in cur:
+        sr.append(
+        	sr(
+				SearchGUID=str(row[0]),
+				Name=str(row[1]),
+        		Description=str(row[2])
+        		)
+    return sr
+
+def create_sr(sr: sr) -> sr:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.uuid4()
+    sr.OutputFormatGuid = did
+    cur.execute(INSERT_SEARCH, (
+		sr.SearchGUID, 
+    	sr.Name,
+    	sr.Description
+    	)
+    )
+    db.connection.commit()
+    return get_sr(did)
+
+def update_sr(sr: sr) -> sr:
+    db = DBConnection()
+    cur = db.get_cursor()
+    did = uuid.UUID(sr.OutputFormatGuid)
+    cur.execute(UPDATE_SEARCH, (did, sr.Name, sr.Description))
+    db.connection.commit()
+    return get_sr(did)
+
+def delete_sr(did: uuid):
+    db = DBConnection()
+    cur = db.get_cursor()
+    cur.execute(DELETE_SEARCH, [did])
+    db.connection.commit()
+    return   
---
+++ main.py
@@ -0,0 +1,170 @@
+import uuid
+
+from fastapi import FastAPI, HTTPException
+from typing import List
+
+from models.document_model import df, dmd, ifm, ofm, api, sr
+from repositories.document_repository import get_all_df, get_all_dmd, get_df, get_dmd, create_df, create_dmd, update_df, update_dmd, delete_df, delete_dmd, purge_dmd, get_all_ifm, get_ifm, create_ifm, update_ifm, delete_ifm, get_all_ofm, get_ofm, create_ofm, update_ofm, delete_ofm, get_all_api, get_api, create_api, update_api, delete_api, purge_api, get_all_ar, get_sr, create_sr, update_sr, delete_sr
+
+app = FastAPI()
+
+#TODO: df and dmd - the insert statements for DMD needs to check to see if the file guid and ifm guid exists before inserting
+#todo: create sql to return GUID exists for all tables then run it for updates inserts
+
+# === get all record 
+@app.get("/df")
+def api_get_all_df() -> List[df]:
+    return get_all_df()
+
+@app.get("/dmd")
+def api_get_all_dmd() -> List[dmd]:
+    return get_all_dmd()
+
+@app.get("/ifm")
+def api_get_all_ifm() -> List[ifm]:
+    return get_all_ifm()
+    
+@app.get("/ofm")
+def api_get_all_ofm() -> List[ofm]:
+    return get_all_ifm()
+
+@app.get("/api")
+def api_get_all_api() -> List[api]:
+    return get_all_api()
+
+@app.get("/sr")
+def api_get_all_sr() -> List[sr]:
+    return get_all_sr()
+
+# === get one record     
+@app.get("/df/{did}")
+def api_get_df(did: str) -> df:
+    did = uuid.UUID(FileMetaDataGUID)
+    df = get_user(did)
+    return document
+
+@app.get("/dmd/{did}")
+def api_get_dmd(did: str) -> dmd:
+    did = uuid.UUID(did)
+    dmd = get_user(did)
+    return dmd
+
+@app.get("/ifm/{did}")
+def get_ifm(did: str) -> ifm:
+    did = uuid.UUID(did)
+    ifm = get_ifm(did)
+    return ifm
+
+@app.get("/ofm/{did}")
+def api_get_ofm(FileMetaDataGUID: str) -> Format:
+    did = uuid.UUID(did)
+    ofm = get_ofm(did)
+    return ofm
+
+@app.get("/api/{did}")
+def api_get_api(did: str) -> api:
+    did = uuid.UUID(did)
+    api = get_ofm(api)
+    return api
+@app.get("/sr/{did}")
+def api_get_sr(did: str) -> sr:
+    did = uuid.UUID(did)
+    sr = get_ofm(sr)
+    return sr
+
+
+#=== create record
+@app.post("/df")
+def api_create_df(df: df) -> List[df]:
+    return create_user(user)
+
+@app.post("/dmd")
+def api_create_dmd(dmd: dmd) -> List[dmd]:
+    return create_dmd()
+
+@app.post("/ifm")
+def api_create_ifm() -> List[ifm]:
+    return create_ifm()
+    
+@app.post("/ofm")
+def api_create_ofm() -> List[ofm]:
+    return create_ofm()
+
+@app.post("/api")
+def api_create_api() -> List[api]:
+    return create_api()
+
+@app.post("/sr")
+def api_create_sr() -> List[sr]:
+    return create_sr()
+
+# update record
+
+@app.put("/df/{did}")
+def api_update_df(did: str,  df:df) -> df:
+    if did != df.fileGuid:
+        raise HTTPException(status_code=400, detail="path id and object id must match")
+    return update_df(did, df)
+
+@app.put("/dmd/{did}")
+def api_update_dmd(did: str,  dmd:dmd) -> dmd:
+    if did != dmd.fileGuid:
+        raise HTTPException(status_code=400, detail="path id and object id must match")
+    return update_dmd(did, dmd)
+
+@app.put("/ifm/{did}")
+def api_update_ifm(did: str,  ifm:ifm) -> ifm:
+    if did != ifm.fileGuid:
+        raise HTTPException(status_code=400, detail="path id and object id must match")
+    return update_ifm(did, ifm)
+
+@app.put("/ofm/{did}")
+def api_update_ofm(did: str,  ofm:ofm -> ofm:
+    if did != ofm.fileGuid:
+        raise HTTPException(status_code=400, detail="path id and object id must match")
+    return update_df(did, df)
+
+@app.put("/api/{did}")
+def api_update_df(did: str,  api:api) -> api:
+    if did != df.fileGuid:
+        raise HTTPException(status_code=400, detail="path id and object id must match")
+    return update_df(did, api)
+
+@app.put("/sr/{did}")
+def api_update_sr(did: str,  sr:sr) -> sr:
+    if did != df.fileGuid:
+        raise HTTPException(status_code=400, detail="path id and object id must match")
+    return update_sr(did, sr)
+
+
+# delete record
+
+@app.delete("/df/{did}")
+def api_delete_df(did: str):
+    did = uuid.UUID(did)
+    delete_user(did)
+
+@app.delete("/dmd/{did}")
+def api_delete_dmd(did: str):
+    did = uuid.UUID(did)
+    delete_user(did)
+
+@app.delete("/ifm/{did}")
+def api_delete_ifm(did: str):
+    did = uuid.UUID(did)
+    delete_user(did)
+
+@app.delete("/ofm/{did}")
+def api_delete_ofm(did: str):
+    did = uuid.UUID(did)
+    delete_user(did)
+
+@app.delete("/api/{did}")
+def api_delete_api(did: str):
+    did = uuid.UUID(did)
+    delete_user(did)
+
+@app.delete("/sr/{did}")
+def api_delete_sr(did: str):
+    did = uuid.UUID(did)
+    delete_user(did)
