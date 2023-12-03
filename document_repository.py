4c02a0cb654e127cc4c1682841140433e006ebd7
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-03 15:00:23 -0600

    Update document_repository.py
    updated spacing

--- document_repository.py
+++ document_repository.py
@@ -360,14 +360,14 @@ def get_all_api() -> List[api]:
     for row in cur:
         api_list.append(
             api(
-            APIGUID=str(row[0]),
-            API=str(row[1]),
+                APIGUID=str(row[0]),
+                API=str(row[1]),
                 OwnerEmailAddress=str(row[2]), 
                 DateExpires=str(row[3]), 
                 PermissionCreate=bool(row[4]),
                 PermissionGlobalAdmin=bool(row[5]),
-               IsDisabled=bool(row[6]),
-               IsDeleted=bool(row[7])
+                IsDisabled=bool(row[6]),
+                IsDeleted=bool(row[7])
                 )
         )
     return api_list    
