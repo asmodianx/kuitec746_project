233e80c1069bd3eb38f0cbf89e54d54d5f6e63ab
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-04 16:49:17 -0600

    Update document_repository.py

--- document_repository.py
+++ document_repository.py
@@ -364,7 +364,18 @@ def get_all_api() -> List[api]:
     cur.execute(SELECT_ALL_API)
 
     for row in cur:
-        api_list.append(api(APIGUID=str(row[0]),API=str(row[1]),OwnerEmailAddress=str(row[2]),DateExpires=str(row[3]),PermissionCreate=str(row[4]),PermissionGlobalAdmin=str(row[5]),IsDisabled=str(row[6]),IsDeleted=str(row[7])))
+        api_list.append(
+            api(
+                APIGUID=str(row[0]),
+                API=str(row[1]),
+                OwnerEmailAddress=str(row[2]),
+                DateExpires=str(row[3]),
+                PermissionCreate=str(row[4]),
+                PermissionGlobalAdmin=str(row[5]),
+                IsDisabled=str(row[6]),
+                IsDeleted=str(row[7])
+            )
+        )
         print(api_list)  
     return api_list    
 def get_api(did: str) -> List[api]:
