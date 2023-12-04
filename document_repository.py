e44559866cc266284cb6c732c6e669ec439b6bd9
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-04 16:29:24 -0600

    Update document_repository.py

--- document_repository.py
+++ document_repository.py
@@ -364,7 +364,7 @@ def get_all_api() -> List[api]:
     cur.execute(SELECT_ALL_API)
 
     for row in cur:
-        print(row)
+        print(row[0])
         api_list.append(
             api(
                 APIGUID=row[0],
