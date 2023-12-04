c8a9be19374f111b4641ab81358a97afebe7e0c0
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-04 10:56:10 -0600

    Update document_model.py

--- document_model.py
+++ document_model.py
@@ -2,51 +2,51 @@ from fastapi_camelcase import CamelModel
 from typing import Optional
 
 class dmd(CamelModel):
-	APIGUID: Optional[str] = None
-	FileMetaDataGUID: Optional[str] = None
-	Name: Optional[str] = None
-	Description: Optional[str] = None
-	DateUploaded: Optional[str] = None
-	DateModified: Optional[str] = None
-	Revision: Optional[str] = None
-	IsCurrent: Optional[bool] = None
-	IsDeleted: Optional[bool] = None
-	FileGuid: Optional[str] = None
-	InputFormatGuid: Optional[str] = None
-	FileHash: Optional[str] = None
-	FileCacheExpiration: Optional[str] = None
+    APIGUID: Optional[str] = None
+    FileMetaDataGUID: Optional[str] = None
+    Name: Optional[str] = None
+    Description: Optional[str] = None
+    DateUploaded: Optional[str] = None
+    DateModified: Optional[str] = None
+    Revision: Optional[str] = None
+    IsCurrent: Optional[bool] = None
+    IsDeleted: Optional[bool] = None
+    FileGuid: Optional[str] = None
+    InputFormatGuid: Optional[str] = None
+    FileHash: Optional[str] = None
+    FileCacheExpiration: Optional[str] = None
 
 class df(CamelModel):
-	fileGuid: Optional[str] = None
-	fileContents: Optional[str] = None
+    fileGuid: Optional[str] = None
+    fileContents: Optional[str] = None
 
 class api(CamelModel):
-	APIDGUID: Optional[str] = None
-	FileMetaDataGUID: Optional[str] = None
-	API: Optional[str] = None
-	OwnerEmailAddress: Optional[str] = None
-	DateExpires: Optional[str] = None
-	PermissionCreate: Optional[str] = None
-	PermissionGlobalAdmin: Optional[str] = None
-	IsDisabled: Optional[bool] = None
-	IsDeleted: Optional[bool] = None
+    APIDGUID: Optional[str] = None
+    FileMetaDataGUID: Optional[str] = None
+    API: Optional[str] = None
+    OwnerEmailAddress: Optional[str] = None
+    DateExpires: Optional[str] = None
+    PermissionCreate: Optional[str] = None
+    PermissionGlobalAdmin: Optional[str] = None
+    IsDisabled: Optional[bool] = None
+    IsDeleted: Optional[bool] = None
 
 class ifm(CamelModel):
-	InputFormatGuid: Optional[str] = None
-	Name: Optional[str] = None
-	Description: Optional[str] = None
-	FileExtension: Optional[str] = None
-	FileMIMEType: Optional[str] = None
+    InputFormatGuid: Optional[str] = None
+    Name: Optional[str] = None
+    Description: Optional[str] = None
+    FileExtension: Optional[str] = None
+    FileMIMEType: Optional[str] = None
 
 class ofm(CamelModel):
-	OutputFormatGuid: Optional[str] = None
-	Name: Optional[str] = None
-	Description: Optional[str] = None
-	FunctionName: Optional[str] = None
-	FileExtension: Optional[str] = None
-	FileMIMEType: Optional[str] = None
+    OutputFormatGuid: Optional[str] = None
+    Name: Optional[str] = None
+    Description: Optional[str] = None
+    FunctionName: Optional[str] = None
+    FileExtension: Optional[str] = None
+    FileMIMEType: Optional[str] = None
 
 class sr(CamelModel):
-	SearchGuid: Optional[str] = None
-	Name: Optional[str] = None
-	Description: Optional[str] = None
+    SearchGuid: Optional[str] = None
+    Name: Optional[str] = None
+    Description: Optional[str] = None
