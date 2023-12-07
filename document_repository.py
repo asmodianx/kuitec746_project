b8e2919aea8340cd3d4e791a2789b5aa44fe2c2c
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-07 11:58:25 -0600

    Update document_repository.py

--- document_repository.py
+++ document_repository.py
@@ -324,7 +324,7 @@ def create_ofm(ofm_create: ofm) -> ofm:
     db = DBConnection()
     cur = db.get_cursor()
     ofm_create.outputFormatGuid = uuid.uuid4()
-    cur.execute(INSERT_OF, ( ofm_create.outputFormatGuid, ofm_create.name, ofm_create.functionName, ofm_create.fileExtension, ofm_create.fileMimeType ))
+    cur.execute(INSERT_OF ( ofm_create.outputFormatGuid, ofm_create.name, ofm_create.functionName, ofm_create.fileExtension, ofm_create.fileMimeType ))
     db.connection.commit()
     return get_ofm(did)
 
