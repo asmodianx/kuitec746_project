7ee0caf6d6672fe8ab42ab8e2f0da43eb41bef68
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-07 20:57:58 -0600

    Update document_repository.py

--- document_repository.py
+++ document_repository.py
@@ -306,8 +306,7 @@ def get_ofm(did: uuid) -> List[ofm]:
     ofm_list = []
     db = DBConnection()
     cur = db.get_cursor()
-    diduuid = uuid.uuid4(did)
-    cur.execute(SELECT_OF,[diduuid])
+    cur.execute(SELECT_OF,[did])
     for row in cur:
         ofm_list.append(
             ofm(
