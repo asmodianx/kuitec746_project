48dab6a4a208ccfdb2bfb291cd4e317e1b428098
Asmodian X <asmodianx@users.noreply.github.com>
2023-12-09 11:33:15 -0600

    Update document_repository.py

--- document_repository.py
+++ document_repository.py
@@ -372,7 +372,7 @@ def create_sr(sr_create: sr) -> sr:
     did = str(uuid.uuid4())
     cur.execute(INSERT_SEARCH, (did, sr_create.name, sr_create.description ))
     db.connection.commit()
-    output = {'result':did}
+    output = [did]
     return output
 
 def update_sr(sr_update: sr) -> sr:
